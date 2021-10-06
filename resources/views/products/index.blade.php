89196735b4949ff4eb076b7cfdea65c27c6f5e83
Your Name <you@example.com>
2021-10-06 13:51:46 +0800

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="container card">
+        <div class="row p-2">
+            <div class="col-sm">
+                <img class="img-fluid"src="{{asset('image\img1.png')}}" alt="">
+            </div>
+            <div class="col-sm">
+                <img class="img-fluid"src="{{asset('image\img2.png')}}" alt="">
+            </div>
+            <div class="col-sm">
+                <img class="img-fluid"src="{{asset('image\img3.png')}}" alt="">
+            </div>
+        </div>
+        <div class="row p-2">
+            <div class="col-sm">
+                <img class="img-fluid"src="{{asset('image\img4.png')}}" alt="">
+            </div>
+            <div class="col-sm">
+                <img class="img-fluid"src="{{asset('image\img5.png')}}" alt="">
+            </div>
+            <div class="col-sm">
+                <img class="img-fluid"src="{{asset('image\img6.png')}}" alt="">
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Products</h1>
+                </div>
+                <div class="col-sm-6">
+                    <a class="btn btn-primary float-right"
+                       href="{{ route('products.create') }}">
+                        Add New
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="content px-3">
+
+        @include('flash::message')
+
+        <div class="clearfix"></div>
+
+        <div class="card">
+            <div class="card-body p-0">
+                @include('products.table')
+
+                <div class="card-footer clearfix float-right">
+                    <div class="float-right">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+@endsection
+
